75c27ff139bfd907033c3d9d4eb5c9ad3dea074c
Sanjeev Yadav <33087026+SanjeevYadavcr7@users.noreply.github.com>
2020-08-16 14:57:00 +0530

    Update bloom_clock.py

--- clock_sync_ds/code/bloom_clock.py
+++ clock_sync_ds/code/bloom_clock.py
@@ -1,4 +1,4 @@
-# referenced from https://towardsdatascience.com/understanding-lamport-timestamps-with-pythons-multiprocessing-library-12a6427881c6
+
 from multiprocessing import Process,Pipe,Manager
 from os import getpid
 from datetime import datetime
